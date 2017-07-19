c81808795bcad7e3c7bec7f82e2d62495f62fd39
foxwest <foxwest@qq.com>
2017-07-19 17:45:31 +0800

    添加后台个人设置自定义样式resources/assests/css/other.css,并用laravel Mix合并到public/css/app.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,4 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+mix.styles(['resources/assets/css/other.css','public/css/app.css'],'public/css/app.css');
