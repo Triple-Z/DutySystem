1d2c721926d5f1f0924130e9147f9f6c46ad05c5
foxnuaaer <foxnuaaer@nuaa.edu.cn>
2017-08-22 21:39:02 +0800

    FEAT(action record)

--- resources/views/superhome.blade.php
+++ resources/views/superhome.blade.php
@@ -21,19 +21,19 @@
     <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
             <ul class="nav nav-sidebar">
-                <li class="active"><a data-toggle="tab" href="#last">上次登录记录</a></li>
-                <li><a data-toggle="tab" href="#history">历史登录记录</a></li>
+                <!-- <li class="active"><a data-toggle="tab" href="#last">上次登录记录</a></li> -->
+                <li class="active"><a data-toggle="tab" href="#history">历史登录记录</a></li>
                 <li><a data-toggle="tab" href="#name">修改用户名</a></li>
                 <li><a data-toggle="tab" href="#password">修改密码</a></li>
             </ul> 
         </div>
         <div class="col-md-8 tab-content">
-            <div class="tab-pane panel panel-default fade active in" id="last">
+            <!-- <div class="tab-pane panel panel-default fade active in" id="last">
                 <div class="panel-heading">上次登录记录</div>
                 <div class="panel-body">
                     You are logged in!
                 </div>
-            </div>
+            </div> -->
             <div class="tab-pane panel panel-default fade" id="email">
                 <div class="panel-heading">修改邮箱</div>
                 <div class="panel-body">
@@ -46,11 +46,33 @@
                     You are logged in!
                 </div>
             </div>
-            <div class="tab-pane panel panel-default fade" id="history">
+            <div class="tab-pane panel panel-default fade active in" id="history">
                 <div class="panel-heading">登录历史</div>
                 <div class="panel-body">
-                    You are logged in!
+                <div class="table-responsive col-md-11">
+                    <table class="table table-striped" id="tableExcel">
+                        <thead style="text-align:center;">
+                            <tr>
+                                <th style="width:30%;">用户</th>
+                                <th>时间</th>
+                                <th>操作</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($actions as $action)
+                                <tr>
+                                    <th>{{ $action->user->name }}</th>
+                                    <th>{{ $action->timestamp }}</th>
+                                    <th>{{ $action->action }}
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <div style="text-align: center;">
+                        {{ $actions->links() }} 
+                    </div>
                 </div>
+            </div>
             </div>
             <div class="tab-pane panel panel-default fade" id="password">
                 <div class="panel-body">
